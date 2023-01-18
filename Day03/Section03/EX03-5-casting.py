f86a7b42cacda93bcbe9ea106efca70ce333616c
ITSC <ITSC@naver.com>
2023-01-18 21:19:21 +0900

    Day03 casting

---
+++ Day03/Section03/EX03-5-casting.py
@@ -0,0 +1,39 @@
+'''
+Casting
+    변수에 유형을 지정하려는 경우 캐스팅으로 가능
+
+'''
+
+#정수형
+x = int(1)
+print(x)
+y = int(2.8)
+print(y) #절삭됨
+z = int("3")
+print(z)
+print(type(z))
+print(x+z)
+
+#실수형
+x = float(1)
+print(x)
+z = float("3")
+print(z)
+
+#문자형
+x = str(1)
+y = str(2)
+print(y)
+print(x+y) #문자열 끼리의 합
+
+
+#아스키코드 변환
+a = ord('A')
+print(a)
+b = chr(65)
+print(b)
+
+
+
+
+
