6f3df85c7c95443facebfa3df5feb368360dcd50
ITSC <ITSC@naver.com>
2023-01-18 21:50:18 +0900

    Day03 arithmetic

---
+++ Day03/Section04/EX04-1-arithmetic.py
@@ -0,0 +1,18 @@
+'''
+
+1. 산술 연산자
+    +, -, *, **, /, //, %
+
+2.
+
+'''
+a = 7
+b = 2
+#덧셈
+print("{} + {} = {}".format(a,b,a + b))
+print("{} - {} = {}".format(a,b,a - b))
+print("{} * {} = {}".format(a,b,a * b))
+print("{} ** {} = {}".format(a,b,a ** b))
+print("{} / {} = {}".format(a,b,a / b))
+print("{} // {} = {}".format(a,b,a // b))
+print("{} % {} = {}".format(a,b,a % b))
