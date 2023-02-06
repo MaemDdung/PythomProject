85bacb06d130cb3cf2deb939c2d5ba91fc90893f
ITSC <ITSC@naver.com>
2023-02-06 21:46:44 +0900

    Day08 Class, Object

--- Day08/Section15/EX15-1-object.py
+++ Day08/Section15/EX15-1-object.py
@@ -11,7 +11,7 @@ object
     예) 학생, 주문, 배송, 대한민국국민
     
 객체 구성
-    
+    초기화용 - 생성자
     속성값 - 변수
     기능 - 메소드(함수)
     
@@ -19,6 +19,9 @@ object
 
 
 class Computer:
+    
+    # 첫번째 매개 변수가 self 이므로 인스턴스 메소드 이다.
+    # self를 제외한 나머지 배개 변수에 실제로 상용될 데이터가 전달된다.
     def set_spec(self, cpu, ram, vga, ssd):
         self.cpu = cpu
         self.ram = ram
@@ -35,3 +38,10 @@ class Computer:
 desktop = Computer()
 desktop.set_spec('i7','18GB','RTX3060', '512GB')
 desktop.hardware_info()
+print()
+desktop.cpu = "i9"
+desktop.hardware_info()
+print()
+mackbook = Computer()
+mackbook.set_spec('M2', '16GB', 'M2', '512GB')
+mackbook.hardware_info()
