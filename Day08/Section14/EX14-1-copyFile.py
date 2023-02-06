6c1a194d3c7b7c15fb1b6ff41cff71d3d96aaf0f
ITSC <ITSC@naver.com>
2023-02-06 21:21:46 +0900

    Day08 copyFile

---
+++ Day08/Section14/EX14-1-copyFile.py
@@ -0,0 +1,24 @@
+'''
+파일 복사
+    원본 -> 버버 편수(memory) -> 복사본
+
+
+'''
+
+buffer_size = 1024  #1024byte -> 1KB의미
+
+with open('../../Day07/Section13/hello.txt', 'rb') as source:
+    with open('hello2.txt','wb') as copy:
+        while 1:
+            buffer = source.read(buffer_size)
+            if not buffer:
+                break
+            copy.write(buffer)
+print("hello2.txt파일이 복사 되었습니다.")
+
+
+
+
+
+
+
